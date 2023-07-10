965c5267f8c4a1f732e0d36cc2b2b3613a8e9ad2
Ravs0nek <MiloszZRT@wp.pl>
2023-07-10 17:45:15 +0200

    next commit

--- src/Api.py
+++ src/Api.py
@@ -7,7 +7,7 @@ from SecondaryFunctions import days, is_weekend
 
 class CurrencyData:
 
-    def exchange_rate_api(currency: str, first_day = days(12)[0], last_day = days(12)[-1]) -> Tuple(list, list, str):
+    def exchange_rate_api(currency: str, first_day = days(12)[0], last_day = days(12)[-1]) -> Tuple[list, list, str]:
         response = requests.get(
             f'http://api.nbp.pl/api/exchangerates/rates/a/{currency}/{first_day}'
             f'/{last_day}')
@@ -18,7 +18,7 @@ class CurrencyData:
             days.append(data['effectiveDate'])
         return exchange_rates, days, currency
     
-    def week_exchange_rate(exchange_rates: Tuple(list, list, str), week_days = days(12)) -> Tuple(list, list): 
+    def week_exchange_rate(exchange_rates: Tuple(list, list, str), week_days = days(12)) -> Tuple[list, list]: 
         rates = exchange_rates[0]
         for i in range(2):
             if is_weekend(week_days[i]):
